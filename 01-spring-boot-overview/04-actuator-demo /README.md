58848b9d02faccb0564212fd8a9b5a8b6db1e20d
pjancura <jancura.paul@gmail.com>
2025-08-14 20:31:54 -0400

    adds files about custom properties, and setting property configurations for your app

--- 01-spring-boot-overview/04-actuator-demo /README.md
+++ 01-spring-boot-overview/04-actuator-demo /README.md
@@ -1,2 +1,4 @@
 # mycoolapp
 This repo will follow a uDemy course by Luv2Code on the Spring Framework
+
+The portion contains information about actuator endpoints, security, and custom properties
--- 01-spring-boot-overview/04-actuator-demo /src/main/java/com/udemy/course/demo/mycoolapp/controller/FunRestController.java
+++ 01-spring-boot-overview/04-actuator-demo /src/main/java/com/udemy/course/demo/mycoolapp/controller/FunRestController.java
@@ -1,12 +1,26 @@
-package com.udemy.course.demo.mycoolapp.rest;
+package com.udemy.course.demo.mycoolapp.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
-    // expose "/" that return "Hello World"
 
+    // examples of custom.properties
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose new endpoint for "teaminfo"
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team Name: " + teamName;
+    }
+
+    // expose "/" that return "Hello World"
     @GetMapping("/")
     public String sayHello() {
         return "Hello World";
